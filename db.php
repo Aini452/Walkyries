4362fc971d4815ae776b758202f5fec9c03943ca
Acapulcohood <adelabre@humanbooster.com>
2019-04-16 11:41:00 +0200

    add some players into table player

--- db.php
+++ db.php
@@ -23,13 +23,46 @@ if($db->connect_errno){
     throw new Exception($db->connect_error);
 }
 
-$db->query('CREATE TABLE IF NOT EXISTS `team`(team_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, team_name VARCHAR(255);');
+
+
+$db->query('CREATE TABLE IF NOT EXISTS `player`(
+    player_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    last_name VARCHAR(255),
+    first_name VARCHAR(255));');
 
 if ($db->errno){
     throw new Exception($db->error);
 }
-$db->query('CREATE TABLE IF NOT EXISTS `player`(player_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, last_name VARCHAR(255), first_name VARCHAR(255)),FOREIGN KEY (team_id) REFERENCES team(team_id);');
 
+$db->query('CREATE TABLE IF NOT EXISTS `team`(
+    team_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    team_name VARCHAR(255),
+    player1_id INT,
+    player2_id INT,
+    FOREIGN KEY (player1_id) REFERENCES player(player_id),
+    FOREIGN KEY (player2_id) REFERENCES player(player_id));');
+
+    
 if ($db->errno){
     throw new Exception($db->error);
 }
+
+if ( !$db->query ("INSERT INTO player (last_name, first_name)  
+                                VALUES  ('Stark', 'Sansa'),
+                                        ('Stark', 'Bran'),
+                                        ('Stark', 'Arya'),
+                                        ('Targaryen', 'Daenerys'),
+                                        ('Targaryen', 'Aegon'),
+                                        ('Lannister', 'Tyrion'),
+                                        ('Lannister', 'Jaime'),
+                                        ('Lannister', 'Cersei'),
+                                        ('King', 'Night'),
+                                        ('Baratheon', 'Gendry'),
+                                        ('De Torth', 'Brienne'),
+                                        ('Fleau D\'Ogre', 'Tormund'),
+                                        ('Clegane', 'Sandor'),
+                                        ('Mormont', 'Jorah'),
+                                        ('Lord', 'Varys');") )                                       
+        {
+                echo "1- Erreur : ".$db->errno." - ".$db->error;
+        }
