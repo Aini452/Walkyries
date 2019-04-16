10ad6a51acb2a37c37a3b80cccedc57f7baf5c5f
Aini452 <amandine.andre04@gmail.com>
2019-04-16 11:08:58 +0200

    merge, the last ?

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -1,15 +1,9 @@
 <?php
 
-<<<<<<< HEAD
-namespace App\Controller;
-=======
->>>>>>> 2f4bf61d94988c8618462ea54eb484eaf08c33c9
-
 namespace App\Controller;
 class TeamController {
     public function create(){
     }
-<<<<<<< HEAD
     
     public function read(){
     }
@@ -21,18 +15,3 @@ class TeamController {
     }
     
 }
-
-
-
-=======
-    
-    public function read(){
-    }
-    
-    public function update(){
-    }
-    
-    public function delete(){
-    }
-}
->>>>>>> 2f4bf61d94988c8618462ea54eb484eaf08c33c9
