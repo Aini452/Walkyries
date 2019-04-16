e4e18e6bd3c7efe066d83052bf3bc87a68a4afd4
hinerava <hpunu@humanbooster.com>
2019-04-16 12:31:51 +0200

    addteam.php

--- src/Model/Team.php
+++ src/Model/Team.php
@@ -6,6 +6,8 @@ Class Team {
 
     private $id;
     private $teamName;
+    private $player1;
+    private $player2;
 
     public function getId(){
         return $this->id;
@@ -22,6 +24,24 @@ Class Team {
     
     public function setTeamName($value){
         $this->teamName = $value;
+        return $this;
+    }
+
+    public function getPlayer1() {
+		return $this->player1;
+    }
+    
+    public function setPlayer1($value){
+        $this->player1 = $value;
+        return $this;
+    }
+
+    public function getPlayer2() {
+		return $this->player2;
+    }
+    
+    public function setPlayer2($value){
+        $this->player2 = $value;
         return $this;
     }
     
