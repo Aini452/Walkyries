c3f03b4fd856bfb4c6f980001ca80e574b5800d6
hinerava <hpunu@humanbooster.com>
2019-04-16 11:02:49 +0200

    merge

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -1,6 +1,5 @@
 <?php
   
-<<<<<<< HEAD
 namespace App\Controller;
 
 class PlayerController{
@@ -8,6 +7,4 @@ class PlayerController{
     
 
 }
-=======
-namespace App\Controller;
->>>>>>> 534616d5cebbdf4e898ed1ef155211990675800d
+
--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -1,23 +1,21 @@
 <?php
 
-<<<<<<< HEAD
 namespace App\Controller;
 
 class TeamController {
+    public function create(){
+    }
+    
+    public function read(){
+    }
+    
+    public function update(){
+    }
+    
+    public function delete(){
+    }
     
 }
-=======
-namespace App\Controller;
->>>>>>> 534616d5cebbdf4e898ed1ef155211990675800d
 
-public function create(){
-}
 
-public function read(){
-}
 
-public function update(){
-}
-
-public function delete(){
-}
